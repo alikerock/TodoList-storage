a10c7135a0fd77b392e9c3e99de37c3787e56aea
unknown <48326361+alikerocknv@users.noreply.github.com>
2025-07-22 17:23:27 +0900

    package json update
    src folder, package.json update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import Todo from './Todo';
 
 function App() {
+  const [todo, setTodo] = useState([
+    {id:1, title:'Learn web', checked:true},
+    {id:2, title:'Get a job', checked:false}
+  ]);
+  const checkUpdate = (id, value)=>{
+    let newtodos = todo.map(item=>{
+      return item.id === id ?  {...item, checked:value} : item
+    });
+    setTodo(newtodos);
+  }
+  const updateTodo = (id, value)=>{
+    console.log(id,value);
+    let newtodos = todo.map(item=>{
+      return item.id === id ?  {...item, title:value} : item
+    });
+    setTodo(newtodos);
+  }
+  const deleteTodo = (id)=>{
+    setTodo(prev=>prev.filter(item=>item.id !== id));
+  }
+
+  const todos = todo.map(item=>
+    <Todo 
+      key={item.id} 
+      data={item} 
+      checkUpdate={checkUpdate}
+      deleteTodo={deleteTodo}
+      updateTodo={updateTodo}
+    />
+  );
+
+  const addTodo = (value) => {
+    setTodo(prev => [
+      ...prev,
+      { id: prev[prev.length - 1].id + 1 || 1, title: value, checked: false }
+    ]);
+  };  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>To Do List</h1>
+      <Form onSubmit={(e)=>{
+        e.preventDefault();
+        const value = e.target.todo.value.trim();
+        if (value) {
+          addTodo(value);
+          e.target.reset();
+        }
+
+      }}>
+        <Form.Group className="mb-3" controlId="todo">
+          <Form.Label>할일 입력</Form.Label>
+          <Form.Control type="text" name="todo" placeholder="할일을 입력해주세요" />
+        </Form.Group>
+        <Button type="submit" variant="primary">입력</Button>
+      </Form>
+      <hr/>
+      {todos}
+
+    </Container>
   );
 }
 
